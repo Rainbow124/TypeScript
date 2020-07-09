bbf70b3c141641f703786def08c0c9f2f0e3fba2
qianwenbo <qianwenbo@myoas.com>
2020-07-09 10:37:28 +0800

    项目初始化

---
+++ client-side/build/webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        filename: "main.js"
+    },
+    resolve: {
+        extensions: [".ts",".tsx",".js"]
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    devtool: process.env.NODE_ENV === 'production' ?  false : 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        stats: 'error-only',
+        compress: false,
+        host: 'locahost',
+        port: 8080
+    },
+    plugins: [
+        new CleanWebpackPlugin ({
+            cleanOnceBeforeBuildPatterns: ['./dist']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/template/index.html'
+        })
+    ]
+}
