da7ccb1052fb04fa602f0c3b393d51e977349c72
Rainbow <rainbow1071@163.com>
2020-08-03 23:28:26 +0800

    模块和命名空间

---
+++ client-side/src/ts-modules/a.ts
@@ -0,0 +1,9 @@
+export interface FuncInterface {
+    name: string,
+    (arg: number): string
+}
+export class classC {
+    constructor(parameters: any) {
+        console.log(parameters)
+    }
+}
